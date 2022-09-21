047ca3d7aa267e5f236cebd6aa978bc56b1de2a6
o d <onurdemirhan@gmail.com>
2022-09-21 03:17:40 +0300

    duzeltme

--- sayisal_sonuc2022.py
+++ sayisal_sonuc2022.py
@@ -5,7 +5,7 @@ headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Ge
 the_folder = os.path.dirname(os.path.abspath(__file__))
 out = os.path.join(the_folder, "sayisal.txt")
 
-#50 tane çünkü çekilen tarih itibarıyla 50 çekiliş var
+#50 tane çünkü çekilen tarih itibarıyla 114 çekiliş var
 for cekilis_no in range(1,114):
     webpage = "https://www.millipiyangoonline.com/sisalsans/drawdetails.sayisaloto." + str(cekilis_no) + ".2022.json"
     with requests.get(webpage, headers=headers) as url:
